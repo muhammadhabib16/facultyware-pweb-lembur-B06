41319d88f68218f094200ff88ecd6a2c2754e25a
muhammadhabib16 <2411522024_muhammad@student.unand.ac.id>
2026-06-22 16:26:15 +0700

    update playwright

--- playwright.config.js
+++ playwright.config.js
@@ -27,8 +27,8 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: 'https://facultyware.habibdev.site/',  
-
+    // baseURL: 'https://facultyware.habibdev.site/',  
+      baseURL:'https://facultyware-pweb-lembur-b06-563116539840.us-central1.run.app/',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
   },
